8c044d0b3450fbafbee1ef34cd092b5d81ae91e5
ozua <34402876+ozua@users.noreply.github.com>
2018-01-02 02:37:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,38 +3,36 @@ console.log(num);
 var count = 9;
 
 function main() {
-	if (count > 0){
-		var usernum = document.getElementById('inum').value;
-		var out = document.getElementById('out');
+    if (count == 0) {
+        alert ('Попытки закончились! Игра будет перезагружена. Число было: ' + num) (location.reload());
+    }
+    var usernum = document.getElementById('inum').value;
+    var out = document.getElementById('out');
 			
-		if (usernum == num) {
-			alert ('Ура вы угадали! Игра будет перезагружена.') (location.reload());
-		}
-		else if (usernum > num) {
-			out.innerHTML = 'Ваше число больше';
-		}
-		else {
-			out.innerHTML = 'Ваше число меньше';
-		}
-		document.getElementById('count').innerHTML = 'Осталось попыток: ' + count + ' из 10';
-		count = count - 1;
-	}
-	else {
-		alert ('Game over! Игра будет перезагружена. Число было: ' + num) (location.reload());
-	}
+    if (usernum == num) {
+	alert ('Ура вы угадали! Игра будет перезагружена.') (location.reload());
+    }
+    else if (usernum > num) {
+	out.innerHTML = 'Ваше число больше';
+    }
+    else {
+	out.innerHTML = 'Ваше число меньше';
+    }
+    document.getElementById('count').innerHTML = 'Осталось попыток: ' + count + ' из 10';
+    count--;
 }
 
 function validate() {
     var evt = event || window.event;
     if (evt.charCode >= 48 && evt.charCode <= 57) {
-		var key = evt.keyCode || evt.which;
-        	key = String.fromCharCode(key);
+	var key = evt.keyCode || evt.which;
+        key = String.fromCharCode(key);
             
-        	var val = document.getElementById("inum").value + key;
-        	if (val > 0 && val < 11) {
-        		return true
-    		} 
-	}
+        var val = document.getElementById("inum").value + key;
+        if (val > 0 && val < 11) {
+        	return true
+    	} 
+    }
     evt.returnValue = false;
     if(evt.preventDefault) evt.preventDefault();
 }
